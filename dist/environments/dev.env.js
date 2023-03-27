cfa96c3bef546e633bfa8026ec88167723669ba1
MacBook Air <macbookair@192.168.1.5>
2023-03-27 11:58:27 +0530

    updated code

--- dist/environments/dev.env.js
+++ dist/environments/dev.env.js
@@ -4,5 +4,5 @@ exports.DevEnvironment = void 0;
 exports.DevEnvironment = {
     db_url: 'mongodb+srv://vg100:vg100@cluster0.bszog.mongodb.net/?retryWrites=true&w=majority',
     jwt_secret: 'secret',
-    uploadPath: "src/uploads",
+    uploadPath: "./src/uploads",
 };
