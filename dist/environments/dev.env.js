299a40bbaa63350fbd5b612ad344e6628b4e612c
MacBook Air <macbookair@192.168.1.5>
2023-03-27 11:52:28 +0530

    updated code

--- dist/environments/dev.env.js
+++ dist/environments/dev.env.js
@@ -3,5 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.DevEnvironment = void 0;
 exports.DevEnvironment = {
     db_url: 'mongodb+srv://vg100:vg100@cluster0.bszog.mongodb.net/?retryWrites=true&w=majority',
-    jwt_secret: 'secret'
+    jwt_secret: 'secret',
+    uploadPath: "src/uploads",
 };
--- dist/environments/prod.env.js
+++ dist/environments/prod.env.js
@@ -3,5 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ProdEnvironment = void 0;
 exports.ProdEnvironment = {
     db_url: 'mongodb+srv://vg100:vg100@cluster0.bszog.mongodb.net/?retryWrites=true&w=majority',
-    jwt_secret: 'prodSecret'
+    jwt_secret: 'prodSecret',
+    uploadPath: "dist/uploads",
 };
